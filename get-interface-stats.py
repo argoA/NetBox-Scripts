ccb9812c186a600efb7191940e04470c95121ffd
argoA <67715541+argoA@users.noreply.github.com>
2023-06-15 09:55:53 -0600

    Create get-interface-stats.py
    Initial creation of script.

---
+++ get-interface-stats.py
@@ -0,0 +1,10 @@
+from extras.scripts import *
+
+class GetMikroTikInterfaceStatsScript(Script):
+  
+  class Meta:
+    name = "Get MikroTik Interface Stats"
+    description = "Pull interface stats for MikroTik devices."
+
+  def run(self):
+    pass
