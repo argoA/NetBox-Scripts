d3ca13afec2e78b05747a3c23018bdca8f9a4759
argoA <67715541+argoA@users.noreply.github.com>
2023-06-15 12:07:52 -0600

    Update get-interface-stats.py

--- get-interface-stats.py
+++ get-interface-stats.py
@@ -3,18 +3,24 @@ from dcim.models import Device
 
 class GetMikroTikInterfaceStatsScript(Script):
   
-  class Meta:
-    name = "Get MikroTik Interface Stats"
-    description = "Pull interface stats for MikroTik devices."
-    field_order = ['user', 'password']
+    class Meta:
+        name = "Get MikroTik Interface Stats"
+        description = "Pull interface stats for MikroTik devices."
+        field_order = ['user', 'password', 'site']
     
-  user = StringVar(
-    description="Username used to log into the devices"
-  )
-  password = StringVar(
-    description="Password used to log into the devices"
-  )
+    user = StringVar(
+        description="Username used to log into the devices"
+    )
+    password = StringVar(
+        description="Password used to log into the devices"
+    )
+    site = ObjectVar(
+        description="Site of the device",
+        
+    )
 
-  def run(self, data, commit):
-    for device in Device.objects.filter(name='Testing-Router'):
-      self.log_success(device)
+    def run(self, data, commit):
+        
+        # Get a list of MikroTik routers
+        for device in Device.objects.filter(site=data['site']):
+            self.log_success(device)
